4d7bd2e4e32f0e1e717ee86268436cfd867bdf57
Furkan-Ahmet-Ozdemir <furkanahmetozdemir@hotmail.com>
2024-03-22 00:44:36 +0300

    TextButtons added

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,63 @@ class BenimUygulamam extends StatelessWidget {
             style: TextStyle(color: Colors.black),
           ),
         ),
-        body: Center(),
+        body: YemekSayfasi(),
+      ),
+    );
+  }
+}
+
+class YemekSayfasi extends StatelessWidget {
+  int corbaNo = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                  ),
+                  // color: Colors.yellow,
+                  // higlightColor: Colors.blue,
+                  // splashColor: Colors.black,
+                  onPressed: () {
+                    print('corbaaaaaaaaaaaaa');
+                    corbaNo = 4;
+                    print(corbaNo);
+                  },
+                  child: Image.asset('assets/corba_$corbaNo.jpg')),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                onPressed: () {
+                  print('yemekkkkkkkkkkkkk');
+                },
+                child: Image.asset('assets/yemek_1.jpg'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                onPressed: () {
+                  print('tatlıııııııııı');
+                },
+                child: Image.asset('assets/tatli_1.jpg'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
